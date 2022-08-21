497dcd8746772a33aba6bd799dffad21ce2926d5
pavel-goncharov <pawellgoncharov@gmail.com>
2022-08-21 12:52:03 +0300

    Added adaptive to header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,27 @@
-import {FC} from 'react';
+import {FC, useEffect, useState} from 'react';
 import classes from 'src/styles/common/Header.module.scss';
-import {headerSvgs, navLinks, navLinkWithArrow} from 'src/static/header';
+import {headerSvgs, navLinks, activeNavLink} from 'src/static/header';
+import Burger from './ui/Burger';
 
 const Header: FC = () => {
-    
+  const isDesktop: boolean = window.innerWidth > 900;
+  const [isShowNavLinks, setIsShowNavLinks] = useState<boolean>(isDesktop);
+  const classNavLinks: string = isShowNavLinks ? classes.navLinks : classes.hidden;
+
   return (
     <div className={classes.header}>
-      <img src={headerSvgs.title.src} alt={headerSvgs.title.alt}/>
-      <ul className={classes.navLinks}>
+      <div className={classes.logo}>
+        <Burger onClick={() => {
+          setIsShowNavLinks(!isShowNavLinks);
+          console.log(isDesktop, isShowNavLinks, classNavLinks);
+        }}/>
+        <img src={headerSvgs.title.src} alt={headerSvgs.title.alt}/>
+      </div>
+      <ul className={classNavLinks}>
         {navLinks.map(navLink => 
           <li className={classes.navLink} key={navLink}>
             <p>{navLink}</p>
-            {navLink === navLinkWithArrow &&
+            {navLink === activeNavLink &&
               <div className={classes.arrow}/>
             }
           </li>
